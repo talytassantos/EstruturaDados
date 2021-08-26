9990eb05c0bdf03e7d9be589908e74cb4d374a90
Talyta Santos <talyta.santos@fatec.sp.gov.br>
2021-08-26 00:57:29 -0300

    atualizando conteudo

---
+++ README.md
@@ -0,0 +1,7 @@
+# Estrutura de Dados
+
+
+
+Este repositório possui conteúdos feitos em aula e atividades desenvolvidas para compor a matéria de **Estrutura de Dados**.
+
+Matéria aplicada no curso de **Análise e Desenvolvimento de Sistemas** da **FATEC Zona Leste**. 
--- RecursividadePotencia/src/controller/PotenciaController.java
+++ RecursividadePotencia/src/controller/PotenciaController.java
@@ -1,5 +1,30 @@
 package controller;
 
 public class PotenciaController {
+	
+	public PotenciaController() {
+		super();
+	}
+	
+	public int pot (int base, int expoente) {
+		int cont = 0;
+		int res = 1;
+		
+		while (cont < expoente) {
+			res = res * base;
+			cont++;
+		}
+		return res;
+	}
+	
+	public int potencia (int base, int expoente) {
+		//Condicao de parada
+		if (expoente == 0) {
+			return 1;
+		} else {
+			expoente = expoente -1;
+			return base * potencia (base, expoente);
+		}
+	}
 
 }
